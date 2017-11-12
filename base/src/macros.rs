cc1621cc73d0132a177a1ae81d3b49e817a486eb
suptalentdev <suptalentdev@gmail.com>
2017-11-12 20:52:43 +0100

    refactor: Deprecate define_global and point to add_extern_module

--- base/src/macros.rs
+++ base/src/macros.rs
@@ -2,7 +2,7 @@
 #[macro_export]
 macro_rules! ice {
     () => ({
-        panic!("ICE: Please report an issue at https://github.com/gluon-lang/gluon/issues")
+        ice!("ICE: Please report an issue at https://github.com/gluon-lang/gluon/issues")
     });
     ($msg:expr) => ({
         panic!(concat!($msg, ". Please report an issue at https://github.com/gluon-lang/gluon/issues"))
