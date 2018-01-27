42143da0578d6403bad5518b57e8fd2a9682158b
suptalentdev <suptalentdev@gmail.com>
2018-01-27 21:57:34 +0500

    Add translation test for variant with multiple literals

--- parser/build.rs
+++ parser/build.rs
@@ -1,6 +1,8 @@
 extern crate lalrpop;
 
 fn main() {
+    ::std::env::set_var("LALRPOP_LANE_TABLE", "disabled");
+
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
         .process_file("src/grammar.lalrpop")
